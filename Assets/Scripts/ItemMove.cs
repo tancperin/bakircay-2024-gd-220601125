c93ec101e663432f8cca196fecfad67116bdcc2a
tancperin <perintanc@gmail.com>
2024-11-20 21:53:54 +0300

    Object Movement
    Added the functionality of moving objects with mouse via ray casts.

---
+++ Assets/Scripts/ItemMove.cs
@@ -0,0 +1,41 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ItemMove : MonoBehaviour
+{
+    GameObject holdItem = null;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, 100.0f))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (holdItem != null)
+                {
+                    holdItem.transform.position = new Vector3(hit.point.x, 5, hit.point.z);
+                    Variables.Object(holdItem).Set("isHold", false);
+                    holdItem = null;
+                }
+                else if (hit.transform.gameObject.tag == "Item")
+                {
+                    holdItem = hit.transform.gameObject;
+                    Variables.Object(holdItem).Set("isHold", true);
+                }
+            }
+
+            if (holdItem != null)
+            {
+                holdItem.transform.position = new Vector3(hit.point.x, 5, hit.point.z);
+            }
+        }
+    }
+}
