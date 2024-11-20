75deb5e2a7fb17a433fc2f33bbebf2034682552f
tancperin <perintanc@gmail.com>
2024-11-20 23:49:52 +0300

    Bonus: Placed Object Animation
    If an object captured in placement area, it rotates and changes size to show that its captured.

--- Assets/Scripts/Matcher.cs
+++ Assets/Scripts/Matcher.cs
@@ -1,16 +1,37 @@
+using Unity.Mathematics;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class Matcher : MonoBehaviour
 {
     GameObject item;
+    public float rotateSpeed = 60f;
+    public float sizeChangeSpeed = 1f;
+    private Vector3 originalScale;
+    private Vector3 maxScale, minScale;
+    private float way = 1f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        item?.transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime);
+        if (item != null)
+        {
+            item.transform.localScale = Vector3.Lerp(item.transform.localScale, item.transform.localScale + Vector3.one * way, sizeChangeSpeed * Time.deltaTime);
+            if (item.transform.localScale.x >= maxScale.x)
+            {
+                way = -1f;
+            }
+            if (item.transform.localScale.x <= minScale.x)
+            {
+                way = 1f;
+            }
+        }
 
     }
 
@@ -19,8 +40,14 @@ public class Matcher : MonoBehaviour
     {
         if (item == null && other.gameObject.CompareTag("Item"))
         {
+            Debug.Log("Item entered");
             item = other.gameObject;
-            item.GetComponent<Rigidbody>().isKinematic = true;
+            item.GetComponent<Rigidbody>().useGravity = false;
+            item.GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
+            item.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            originalScale = item.transform.localScale;
+            maxScale = originalScale * 1.1f;
+            minScale = originalScale * 0.5f;
         }
     }
 
@@ -29,7 +56,9 @@ public class Matcher : MonoBehaviour
     {
         if (item != null && other.gameObject.CompareTag("Item"))
         {
-            item.GetComponent<Rigidbody>().isKinematic = false;
+            Debug.Log("Item exited");
+            item.GetComponent<Rigidbody>().useGravity = true;
+            item.transform.localScale = originalScale;
             item = null;
         }
     }
